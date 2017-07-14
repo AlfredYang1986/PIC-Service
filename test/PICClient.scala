af95c76c89cf226e5038943c8eba56c9b698a15e
Unknown <jeorch@foxmail.com>
2017-07-14 17:45:13 +0800

    自动化测试测试函数运行无异常
    自动化测试测试函数运行无异常

--- test/PICClient.scala
+++ test/PICClient.scala
@@ -4,6 +4,8 @@ import play.api.libs.json.JsValue
 import play.api.libs.json.Json.toJson
 import play.api.libs.ws.WSClient
 
+import play.api.test._
+
 import scala.concurrent.{ExecutionContext, Future}
 
 /**
@@ -17,41 +19,85 @@ class PICClient(ws: WSClient, baseUrl: String)(implicit ec: ExecutionContext) {
             .withHeaders("Accept" -> "application/json", "Content-Type" -> "application/json")
             .post(toJson(Map("user_name" -> name, "pwd" -> pwd)))
             .map { response =>
-                println(response.json)
                 (response.json \ "result").get.asOpt[Map[String,JsValue]].get.get("auth_token").get.asOpt[String].get
             }
     }
 
-    def conditionSearchTest(token : String, condition : Map[String, JsValue], skip : Int, contains : String) : Future[Int] = {
-        ws.url(baseUrl + "/data/search")
-            .withHeaders("Accept" -> "application/json","Content-Type" -> "application/json")
-                .post(toJson(Map("token" -> toJson(token),"condition" -> toJson(condition),"skip" -> toJson(skip))))
-                .map{   response =>
-                    println(response.json)
-                    var result = 0
-                    val pages = (response.json \ "page").get.asOpt[List[Map[String,JsValue]]].get.head.get("TOTLE_PAGE").get.asOpt[Int].get
-                    if (pages > 0){
-                        if ((response.json \ "search_result").asOpt[List[Map[String,JsValue]]].get.filterNot(x => x.get("html").get.asOpt[String].get.contains(contains)).length>0){
-                            result = 1
+    def getCateTest() : Future[Map[String,JsValue]] = {
+        ws.url(baseUrl + "/category")
+            .withHeaders("Accept" -> "application/json", "Content-Type" -> "application/json")
+            .post(toJson("test"))
+            .map { response =>
+                (response.json \ "result").get.asOpt[Map[String,JsValue]].get
+            }
+    }
+
+    def conditionSearchResult(token : String, conditions : List[Map[String, JsValue]], skip : Int, contains : String) : List[Future[String]] = {
+
+        val resultList = conditions.map { condition =>
+            val test = ws.url(baseUrl + "/data/search")
+                .withHeaders("Accept" -> "application/json", "Content-Type" -> "application/json")
+                .post(toJson(Map("token" -> toJson(token), "condition" -> toJson(condition), "skip" -> toJson(skip))))
+                .map { response =>
+                    val result = new StringBuffer("ok")
+                    val pages = (response.json \ "page").get.asOpt[List[Map[String, JsValue]]].get.head.get("TOTLE_PAGE").get.asOpt[Int].get
+                    if (pages > 0) {
+                        if ((response.json \ "search_result").asOpt[List[Map[String, JsValue]]].get.filterNot(x => x.get("html").get.asOpt[String].get.contains(contains)).length > 0) {
+                            result.append(s"Error with condition=${condition} with contain=${contains}")
                         }
-                        if (result==0){
-                            for (i <- 1 to pages){
+                        if (result == "ok") {
+                            for (i <- 1 to pages) {
                                 ws.url(baseUrl + "/data/search")
-                                    .withHeaders("Accept" -> "application/json","Content-Type" -> "application/json")
-                                    .post(toJson(Map("token" -> toJson(token),"condition" -> toJson(condition),"skip" -> toJson(i))))
-                                    .map{   response_ =>
-                                        if ((response_.json \ "search_result").asOpt[List[Map[String,JsValue]]].get.filterNot(x => x.get("html").get.asOpt[String].get.contains(contains)).length>0){
-                                            result = 1
+                                    .withHeaders("Accept" -> "application/json", "Content-Type" -> "application/json")
+                                    .post(toJson(Map("token" -> toJson(token), "condition" -> toJson(condition), "skip" -> toJson(i))))
+                                    .map { response_ =>
+                                        if ((response_.json \ "search_result").asOpt[List[Map[String, JsValue]]].get.filterNot(x => x.get("html").get.asOpt[String].get.contains(contains)).length > 0) {
+                                            result.append(s"Error with condition=${condition} with contain=${contains}")
                                         }
                                     }
                             }
                         }
-                        result
-                    }else{
+                        result.toString
+                    } else {
                         println("--> No Results! <--")
-                        result = 1
-                        result
+                        result.append(s"Error with No SearchResults! with with condition=${condition} with contain=${contains}!")
+                        result.toString
                     }
                 }
+            test
+        }
+        resultList
+
+    }
+
+    def oneCondition(token : String, condition : Map[String, JsValue],url:String):Future[String]={
+        ws.url(baseUrl+url)
+            .withHeaders("Accept" -> "application/json","Content-Type" -> "application/json")
+            .post(toJson(Map("token" -> toJson(token), "condition" -> toJson(condition))))
+            //        .map(response=>(response.json \ "status").asOpt[String].get)
+            .map(response=>testUtil.resultHandling(response,condition))
+    }
+
+    def getToken(name : String, pwd : String) : Future[(String,JsValue)] = {
+        ws.url(baseUrl + "/auth/password")
+            .withHeaders("Accept" -> "application/json", "Content-Type" -> "application/json")
+            .post(toJson(Map("user_name" -> name, "pwd" -> pwd)))
+            .map { response =>
+                val res=(response.json \ "result" \ "auth_token").asOpt[String].get
+                ("token",toJson(res))
+            }
+    }
+    def getCategoryTestInfo(categoryTestInfo:String):Future[List[(String,JsValue)]]={
+        ws.url(baseUrl+"/category")
+            .withHeaders("Accept" -> "application/json", "Content-Type" -> "application/json")
+            .post(toJson(Map("" -> "")))
+            .map(response=>{
+                (response.json \ "result" \ categoryTestInfo).asOpt[List[String]].get
+                    .map{v=>
+                        val l:List[String]=(v)::Nil
+                        "category" -> toJson(l)
+                    }
+            }
+            )
     }
 }
